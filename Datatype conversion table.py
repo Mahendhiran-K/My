982ef67bb5e59840f8651f8b7e6963f29404bc6a
Mahendhiran-K <61958535+Mahendhiran-K@users.noreply.github.com>
2020-03-13 16:24:31 +0530

    13032020-Task

---
+++ Datatype conversion table.py
@@ -0,0 +1,80 @@
+#int to str,float,bool#
+
+
+a=5
+
+b=str(a)
+
+c= float(a)
+
+d = bool(a)
+
+print("output of conversion str is",b)
+print("output of conversion float is",c)
+print("output of conversion bool is",d)
+
+
+
+#str to other#
+
+a = "mahi"
+b = int(a)
+c = float(a)
+d = bool(a)
+
+#Additional input for integer#
+
+f = "10"
+i = int(f)
+
+print("output of conversion integer is",b)
+print("output of conversion floatis",c)
+print("output of conversion bool is",d)
+print("output of conversion by giving integer input is",i)
+
+
+#float to other#
+
+a = 5.789
+b = int(a)
+c = str(a)
+d = bool(a)
+print("output of conversion integer is",b)
+print("output of conversion string is",c)
+print("output of conversion bool is",d)
+
+
+#bool to other#
+
+a = True
+
+b = int(a)
+c=str(a)
+d=float(a)
+print("output of conversion integer is",b)
+print("output of conversion string is",c)
+print("output of conversion float is",d)
+
+
+#Sum ==> out put as string#
+
+r = 3.2
+
+a = 22/7*r**2
+
+b=str(a)
+
+print("output of conversion String is",b)
+
+#Sum ==> out put as integer#
+
+r = 3.8
+
+a = 2*22/7*r
+
+b=int(a)
+
+print("output of conversion integer is",b)
+
+
+
