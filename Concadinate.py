d609b05eb4166f0fe527420eb5eba1af2fdee042
Mahendhiran-K <61958535+Mahendhiran-K@users.noreply.github.com>
2020-03-23 11:49:40 +0530

    Add files via upload

---
+++ Concadinate.py
@@ -0,0 +1,20 @@
+
+#TASK1#
+
+a = input("enter the string value :")
+b = input("enter the string value :")
+c = a + b
+d = len(c)
+print(c)
+print(d)
+print(a[0]+a[-1]+b[0]+b[-1])
+
+
+#task2#
+
+a = input("enter the string from user :")
+b = a[0:-1]
+c = a[-1].upper()
+print(b+c)
+
+
