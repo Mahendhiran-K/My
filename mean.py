c30fcf946d38ca0b3cc1c21d721febcbea57640b
Mahendhiran-K <61958535+Mahendhiran-K@users.noreply.github.com>
2020-03-31 09:25:42 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,39 @@
+'''
+a = [3,4,5,8,9,10,12]
+#mean
+
+mean = sum(a) / len(a)
+
+print (mean)
+
+#median
+
+a.sort()
+
+mid = a [len(a)// 2]
+
+print(mid)
+
+#find AVG ,LAst,Middle
+
+avg =  a[0]+mid+a[-1] /3
+
+print(avg)
+
+#hove(first sting first char+last char+last string first charac +last Char
+
+list1 = ["hello","python","world","computer","value"]
+
+a = list1[0]
+
+b = list1[-1]
+
+print(a[0]+a[-1]+b[0]+b[-1])
+'''
+
+
+
+
+
+
+
